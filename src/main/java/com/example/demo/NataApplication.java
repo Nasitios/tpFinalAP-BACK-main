601888735ef6453b38064655a24a53d4287a8187
Nasitios <nasitios@gmail.com>
2022-07-22 17:34:50 -0300

    Initial commit

---
+++ src/main/java/com/example/demo/NataApplication.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@SpringBootApplication
+public class NataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NataApplication.class, args);
+	}
+
+	//@Bean
+	/*public CorsFilter corsFilter() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(Boolean.TRUE);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://primerporfolioweb.web.app"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin","Acces-Control-Allow-Origin","Content-Type","Accept","Authorization","Origin, Accept", "X-Requested-With", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin","Content-Type","Accept","Authorization","Acces-Control-Allow-Origin","Acces-Control-Allow-Credentials"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS","HEAD")); 
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		
+		return new CorsFilter(urlBasedCorsConfigurationSource);
+	}
+*/
+}
